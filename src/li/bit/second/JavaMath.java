e15d8342e6563d77fbfdd1f1f09e2f7d7b026c51
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-07-19 15:29:28 +0300

    daryta 2019-07-19

---
+++ src/li/bit/second/JavaMath.java
@@ -0,0 +1,18 @@
+package li.bit.second;
+
+public class JavaMath {
+    public static void main(String[] args) {
+        int numberOne = 12;
+        int numberTwo = 24;
+        System.out.println(Math.pow(numberOne, 2));
+        System.out.println(Math.cos(numberTwo));
+        System.out.println(Math.sin(numberTwo));
+        System.out.println(Math.log(numberTwo));
+
+
+
+
+
+
+    }
+}
